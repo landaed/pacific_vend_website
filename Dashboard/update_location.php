373d41c858e43aa9d2de0bd14281dcb083e97484
admin <landaed96@gmail.com>
2024-02-22 15:37:13 -0800

    fixed modal

--- Dashboard/update_location.php
+++ Dashboard/update_location.php
@@ -3,13 +3,13 @@
 require_once 'db_connect.php';
 
 // Check if the form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['name']) && isset($_POST['address'])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['original_name']) && isset($_POST['original_address'])) {
     // SQL query to update the location
     $sql = "UPDATE Locations SET name=?, address=?, city=?, ZipCode=?, province=?, Territory=?, type=?, email=?, PhoneNumber=? WHERE name=? AND address=?";
 
     if($stmt = $db->prepare($sql)){
         // Bind variables to the prepared statement as parameters
-        $stmt->bind_param("ssssssssss", $_POST['name'], $_POST['address'], $_POST['city'], $_POST['ZipCode'], $_POST['Province'], $_POST['Territory'], $_POST['type'], $_POST['email'], $_POST['phone_number'], $_POST['name'], $_POST['address']);
+        $stmt->bind_param("sssssssssss", $_POST['name'], $_POST['address'], $_POST['city'], $_POST['ZipCode'], $_POST['Province'], $_POST['Territory'], $_POST['type'], $_POST['email'], $_POST['phone_number'], $_POST['original_name'], $_POST['original_address']);
 
         // Attempt to execute the prepared statement
         if($stmt->execute()){
