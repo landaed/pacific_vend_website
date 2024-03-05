679781a4c60e271a91cc8833a97ddf234ac15c8c
admin <landaed96@gmail.com>
2024-03-05 14:51:53 -0800

    added topbar location search

--- Dashboard/topbar.js
+++ Dashboard/topbar.js
@@ -206,6 +206,44 @@ document.getElementById('topbarContainer').innerHTML = `
 </nav>
 `;
 
+var locationInput = document.getElementById('locationInput');
+var suggestionsBox = document.getElementById('locationSuggestions');
+var locationID = document.getElementById('location_id');
+console.log("loaded machine suggestions");
+if (locationInput) {
+    locationInput.addEventListener('input', function() {
+        console.log("recieved input!");
+        var searchTerm = this.value;
+        if (searchTerm.length > 1) {
+            fetch(`location_suggestions.php?search=${searchTerm}`)
+                .then(response => response.json())
+                .then(data => {
+                    suggestionsBox.innerHTML = '';
+                    data.forEach(location => {
+                        var suggestion = document.createElement('div');
+                        suggestion.textContent = location.Name + ",  " + location.Address;
+                        suggestion.name = location.LocationID;
+                        suggestion.classList.add('suggestion');
+                        suggestion.addEventListener('click', function() {
+                            locationInput.value = this.textContent;
+                            suggestionsBox.innerHTML = '';
+                            locationID.value = this.name;
+                        });
+                        suggestionsBox.appendChild(suggestion);
+                    });
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+        } else {
+            suggestionsBox.innerHTML = '';
+        }
+    });
+}
+else{
+  console.log("ERROR: cant find location input!");
+}
+
 
 var searchButton = document.getElementById('searchButton');
 var locationIDInput = document.getElementById('location_id'); // or use 'locationInput' if that's what stores the location ID
