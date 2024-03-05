8f57f22b82f82239eab2cd5717ef0052a66b29c7
admin <landaed96@gmail.com>
2024-03-05 14:28:06 -0800

    added topbar location search

--- Dashboard/topbar.js
+++ Dashboard/topbar.js
@@ -11,8 +11,9 @@ document.getElementById('topbarContainer').innerHTML = `
     <form
         class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
         <div class="input-group">
-            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
+            <input type="text" id="locationInput" class="form-control bg-light border-0 small" placeholder="Search for..."
                 aria-label="Search" aria-describedby="basic-addon2">
+            <div id="locationSuggestions" class="suggestions-box"></div>
             <div class="input-group-append">
                 <button class="btn btn-primary" type="button">
                     <i class="fas fa-search fa-sm"></i>
@@ -200,4 +201,6 @@ document.getElementById('topbarContainer').innerHTML = `
 
     </ul>
 
-</nav>`;
+</nav>
+<script src="./js/location_suggestions.js"></script>
+`;
