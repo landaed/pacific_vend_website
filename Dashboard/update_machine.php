2eb556bcfacbf9859df359be2055ee16f7dcc78f
admin <landaed96@gmail.com>
2024-03-05 14:13:02 -0800

    fixed update machine modal

--- Dashboard/update_machine.php
+++ Dashboard/update_machine.php
@@ -5,13 +5,16 @@ require_once 'db_connect.php';
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['machine_id'])) {
     // Prepare an update statement
-    $sql = "UPDATE Machines SET LegacyID=?, CIDNumber=?, SerialNumber=?, Name=?, Genre=?, Description=?, Dimensions=?, Supplier=?, PurchasePrice=?, PurchaseDate=?, SalePrice=?, SaleDate=?, SoldTo=? WHERE MachineID=?";
+    $sql = "UPDATE Machines SET Manufacture=?, Model=?, LegacyID=?, CIDNumber=?, SerialNumber=?, Name=?, Genre=?, Description=?, Dimensions=?, Supplier=?, PurchasePrice=?, PurchaseDate=?, SalePrice=?, SaleDate=?, SoldTo=? WHERE MachineID=?";
 
     if($stmt = $db->prepare($sql)){
         // Bind variables to the prepared statement as parameters
-        $stmt->bind_param("sssssssssssssi", $param_legacy_id, $param_cid_number, $param_serial_number, $param_name, $param_genre, $param_description, $param_dimensions, $param_supplier, $param_purchase_price, $param_purchase_date, $param_sale_price, $param_sale_date, $param_sold_to, $param_machine_id);
+        $stmt->bind_param("sssssssssssssssi", $param_manufacture, $param_model, $param_legacy_id, $param_cid_number, $param_serial_number, $param_name, $param_genre, $param_description, $param_dimensions, $param_supplier, $param_purchase_price, $param_purchase_date, $param_sale_price, $param_sale_date, $param_sold_to, $param_machine_id);
 
         // Set parameters
+        $param_machine_id = $_POST['machine_id'];
+        $param_manufacture = $_POST['manufacture'];
+        $param_model = $_POST['model'];
         $param_legacy_id = $_POST['legacy_id'];
         $param_cid_number = $_POST['cid_number'];
         $param_serial_number = $_POST['serial_number'];
@@ -25,8 +28,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['machine_id'])) {
         $param_sale_price = $_POST['sale_price'];
         $param_sale_date = $_POST['sale_date'];
         $param_sold_to = $_POST['sold_to'];
-        $param_machine_id = $_POST['machine_id'];
-
         // Attempt to execute the prepared statement
         if($stmt->execute()){
             // Redirect to a confirmation page if the query was successful
