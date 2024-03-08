1d44062e988e07c2e45bd425e84a6c6f9d86f48c
admin <landaed96@gmail.com>
2024-03-08 11:00:52 -0800

    added edit to machine type

--- Dashboard/add_machine_type.php
+++ Dashboard/add_machine_type.php
@@ -14,7 +14,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $stmt = $db->prepare($sql);
 
         // Bind variables to the prepared statement as parameters
-        $stmt->bind_param("sssssssssssssss", $param_manufacture, $param_model, $param_legacy_id, $param_cid_number, $param_serial_number, $param_name, $param_genre, $param_description, $param_dimensions, $param_supplier, $param_purchase_price, $param_purchase_date, $param_sale_price, $param_sale_date, $param_sold_to);
+        $stmt->bind_param("sssss", $param_manufacture, $param_model, $param_name, $param_genre, $param_dimensions, );
 
         // Set parameters
         $param_manufacture = $_POST['manufacture'];
@@ -26,50 +26,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $stmt->execute();
         $stmt->close();
 
-        // Get the last inserted id
-        $last_inserted_machine_id = $db->insert_id;
+
         // Commit the transaction
         $db->commit();
     }
     catch (Exception $e) {
         // An exception has occurred, which means something went wrong
         $db->rollback();
-        echo "ERROR: Could not execute query (add machine failed): " . $e->getMessage();
+        echo "ERROR: Could not execute query (add machine type failed): " . $e->getMessage();
     }
-    $db->begin_transaction();
-    try{
-        // Now insert into MachineHistory table
-        $sql_history = "INSERT INTO MachineHistory (MachineID, LocationID, StartDate, EndDate) VALUES (?, ?, ?, ?)";
-        $stmt_history = $db->prepare($sql_history);
-
-        // Bind parameters
-        $stmt_history->bind_param("iiss", $last_inserted_machine_id, $param_location_id, $param_start_date, $param_end_date);
-
-        // Set parameters
-        $param_location_id = $_POST['location_id'];
-        $param_start_date = $_POST['start_date'];  // Assuming this is coming from your form
-        $param_end_date = $_POST['end_date'];      // Assuming this is coming from your form, can be NULL
-
-        // Execute the statement
-        $stmt_history->execute();
-        $stmt_history->close();
-
-        // Commit the transaction
-        $db->commit();
-
-        // Redirect to a confirmation page if the query was successful
-        header("Location: add_machine_type_success.html");
-        exit(); // Ensure no further execution of the script after redirect
-    } catch (Exception $e) {
-        // An exception has occurred, which means something went wrong
-        $db->rollback();
-        echo "ERROR: Could not execute query (add location failed): " . $e->getMessage();
-        echo "location id:" . $param_location_id;
-    }
-} else {
-    echo "ERROR: Invalid request";
 }
-
 // Close connection
 $db->close();
 ?>
