56b40c7f920605705421a96967bf33755a5e365b
admin <landaed96@gmail.com>
2024-03-08 11:51:15 -0800

    fix suggestion box

--- Dashboard/js/machine_type_suggestions.js
+++ Dashboard/js/machine_type_suggestions.js
@@ -1,5 +1,5 @@
 var machineTypeInput = document.getElementById('machineTypeInput');
-var suggestionsBox = document.getElementById('machineTypeSuggestions');
+var MachineTypeSuggestionBox = document.getElementById('machineTypeSuggestions');
 var machineTypeID = document.getElementById('machine_type_id');
 console.log("loaded machine suggestions");
 if (machineTypeInput) {
@@ -10,7 +10,7 @@ if (machineTypeInput) {
             fetch(`machine_type_suggestions.php?search=${searchTerm}`)
                 .then(response => response.json())
                 .then(data => {
-                    suggestionsBox.innerHTML = '';
+                    MachineTypeSuggestionBox.innerHTML = '';
                     data.forEach(machineType => {
                         var suggestion = document.createElement('div');
                         suggestion.textContent = machineType.Name;
@@ -24,17 +24,17 @@ if (machineTypeInput) {
                         suggestion.classList.add('suggestion');
                         suggestion.addEventListener('click', function() {
                             machineTypeInput.value = this.textContent;
-                            suggestionsBox.innerHTML = '';
+                            MachineTypeSuggestionBox.innerHTML = '';
                             machineTypeID.value = this.name;
                         });
-                        suggestionsBox.appendChild(suggestion);
+                        MachineTypeSuggestionBox.appendChild(suggestion);
                     });
                 })
                 .catch(error => {
                     console.error('Error:', error);
                 });
         } else {
-            suggestionsBox.innerHTML = '';
+            MachineTypeSuggestionBox.innerHTML = '';
         }
     });
 }
