cc88fd1b2ad5154be58b4a07bfdab71dba223440
admin <landaed96@gmail.com>
2024-03-08 11:35:29 -0800

    made machines list display correct info about their type

--- Dashboard/get_machines_at_location.php
+++ Dashboard/get_machines_at_location.php
@@ -4,20 +4,22 @@ require_once 'db_connect.php';
 
 header('Content-Type: application/json');
 
-// Get the LocationID from the request
 $locationID = isset($_GET['locationID']) ? $_GET['locationID'] : '';
 
 if ($locationID) {
-    // SQL query to select machines that are currently located at the specified location
-    $sql = "SELECT m.*, mh.StartDate, mh.EndDate, l.LocationID
+    $sql = "SELECT m.MachineID, m.LegacyID, m.CIDNumber, m.SerialNumber, m.Description,
+            m.Supplier, m.PurchasePrice, m.PurchaseDate, m.SalePrice, m.SaleDate, m.SoldTo,
+            mt.Name AS MachineTypeName, mt.Model AS MachineTypeModel,
+            mt.Manufacture AS MachineTypeManufacture, mt.Genre AS MachineTypeGenre,
+            mt.Dimensions AS MachineTypeDimensions
             FROM Machines m
+            JOIN MachineType mt ON m.MachineTypeID = mt.MachineTypeID
             JOIN MachineHistory mh ON m.MachineID = mh.MachineID
-            JOIN Locations l ON mh.LocationID = l.LocationID
-            WHERE l.LocationID = ? AND mh.EndDate IS NULL
+            WHERE mh.LocationID = ? AND mh.EndDate IS NULL
             ORDER BY mh.StartDate DESC";
 
     $stmt = $db->prepare($sql);
-    $stmt->bind_param("i", $locationID); // "i" indicates the type is an integer
+    $stmt->bind_param("i", $locationID);
     $stmt->execute();
     $result = $stmt->get_result();
 
