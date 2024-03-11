b9949160c65b2dd5c95ab276d54d0f028eb750af
admin <landaed96@gmail.com>
2024-03-11 12:25:06 -0700

    fixing machine edit modal

--- Dashboard/update_machine.php
+++ Dashboard/update_machine.php
@@ -5,23 +5,17 @@ require_once 'db_connect.php';
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['machine_id'])) {
     // Prepare an update statement
-    $sql = "UPDATE Machines SET Manufacture=?, Model=?, LegacyID=?, CIDNumber=?, SerialNumber=?, Name=?, Genre=?, Description=?, Dimensions=?, Supplier=?, PurchasePrice=?, PurchaseDate=?, SalePrice=?, SaleDate=?, SoldTo=? WHERE MachineID=?";
-
+    $sql = "UPDATE Machines SET MachineTypeID=?, LegacyID=?, CIDNumber=?, SerialNumber=?, Description=?, Supplier=?, PurchasePrice=?, PurchaseDate=?, SalePrice=?, SaleDate=?, SoldTo=? WHERE MachineID=?";
     if($stmt = $db->prepare($sql)){
         // Bind variables to the prepared statement as parameters
-        $stmt->bind_param("sssssssssssssssi", $param_manufacture, $param_model, $param_legacy_id, $param_cid_number, $param_serial_number, $param_name, $param_genre, $param_description, $param_dimensions, $param_supplier, $param_purchase_price, $param_purchase_date, $param_sale_price, $param_sale_date, $param_sold_to, $param_machine_id);
-
+        $stmt->bind_param("issssssssssi", $param_machine_type, $param_legacy_id, $param_cid_number, $param_serial_number, $param_description, $param_supplier, $param_purchase_price, $param_purchase_date, $param_sale_price, $param_sale_date, $param_sold_to, $param_machine_id);
         // Set parameters
         $param_machine_id = $_POST['machine_id'];
-        $param_manufacture = $_POST['manufacture'];
-        $param_model = $_POST['model'];
+        $param_machine_type = $_POST['machine_type_id'];
         $param_legacy_id = $_POST['legacy_id'];
         $param_cid_number = $_POST['cid_number'];
         $param_serial_number = $_POST['serial_number'];
-        $param_name = $_POST['name'];
-        $param_genre = $_POST['genre'];
         $param_description = $_POST['description'];
-        $param_dimensions = $_POST['dimensions'];
         $param_supplier = $_POST['supplier'];
         $param_purchase_price = $_POST['purchase_price'];
         $param_purchase_date = $_POST['purchase_date'];
