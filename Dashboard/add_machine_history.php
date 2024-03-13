8bf449d3456db7af32d89d0dbcedc75663497b15
admin <landaed96@gmail.com>
2024-03-12 20:28:19 -0700

    fixing add machine history endpoint

--- Dashboard/add_machine_history.php
+++ Dashboard/add_machine_history.php
@@ -4,34 +4,35 @@ require_once 'db_connect.php';
 
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $db->begin_transaction();
+  try{
+      // Prepare an insert statement
+      $sql = "INSERT INTO MachineHistory (MachineID, LocationID, StartDate, EndDate) VALUES (?, ?, ?, ?)";
 
-    // Prepare an insert statement
-    $sql = "INSERT INTO MachineHistory (MachineID, LocationID, StartDate, EndDate) VALUES (?, ?, ?, ?)";
+      $stmt = $db->prepare($sql);
+      // Bind variables to the prepared statement as parameters
+      $stmt->bind_param("iiss", $param_machine_id, $param_location_id, $param_start_date, $param_end_date);
 
-    if ($stmt = $db->prepare($sql)) {
-        // Bind variables to the prepared statement as parameters
-        $stmt->bind_param("iiss", $param_machine_id, $param_location_id, $param_start_date, $param_end_date);
+      // Set parameters
+      $param_machine_id = $_POST['machineIDInput'];
+      $param_location_id = $_POST['locationInput'];
+      $param_start_date = $_POST['start_date'];
+      $param_end_date = isset($_POST['end_date']) ? $_POST['end_date'] : NULL;
 
-        // Set parameters
-        $param_machine_id = $_POST['machineIDInput'];
-        $param_location_id = $_POST['locationInput'];
-        $param_start_date = $_POST['start_date'];
-        $param_end_date = isset($_POST['end_date']) ? $_POST['end_date'] : NULL;
+      // Execute the prepared statement
+      $stmt->execute();
+      $stmt->close();
 
-        // Attempt to execute the prepared statement
-        if ($stmt->execute()) {
-            // Redirect to a confirmation page if the query was successful
-            header("Location: add_machine_history_success.html");
-            exit(); // Ensure no further execution of the script after redirect
-        } else {
-            echo "ERROR: Could not execute query: $sql. " . $db->error;
-        }
-    } else {
-        echo "ERROR: Could not prepare query: $sql. " . $db->error;
+
+      // Commit the transaction
+      $db->commit();
+      header("Location: add_machine_history_success.html");
     }
-
-    // Close statement
-    $stmt->close();
+    catch (Exception $e) {
+        // An exception has occurred, which means something went wrong
+        $db->rollback();
+        echo "ERROR: Could not execute query (add machine history failed): " . $e->getMessage();
+    }
 }
 
 // Close connection
