b562bfa48e33703dddec81e612e4eff920a9c0b8
DESKTOP-TJFGPKO\Eli Landa <elanda@wooplagaming.com>
2024-03-18 11:05:56 -0700

    changed get machines at location php

--- Dashboard/add_machine_history.php
+++ Dashboard/add_machine_history.php
@@ -7,18 +7,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $db->begin_transaction();
   try{
       // Prepare an insert statement
-      $sql = "INSERT INTO MachineHistory (MachineID, LocationID, StartDate, EndDate) VALUES (?, ?, ?, ?)";
+      $sql = "INSERT INTO MachineHistory (MachineID, LocationID, StartDate) VALUES ( ?, ?, ?)";
 
       $stmt = $db->prepare($sql);
       // Bind variables to the prepared statement as parameters
-      $stmt->bind_param("iiss", $param_machine_id, $param_location_id, $param_start_date, $param_end_date);
+      $stmt->bind_param("iis", $param_machine_id, $param_location_id, $param_start_date, $param_end_date);
 
       // Set parameters
       $param_machine_id = $_POST['machineIDInput'];
 
       $param_location_id = $_POST['locationIDInput'];
       $param_start_date = $_POST['start_date'];
-      $param_end_date = isset($_POST['end_date']) ? $_POST['end_date'] : NULL;
 
       // Execute the prepared statement
       $stmt->execute();
