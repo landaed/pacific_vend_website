08ddae79881d8ab868ef2b2adc711731366a2dc6
DESKTOP-TJFGPKO\Eli Landa <elanda@wooplagaming.com>
2024-03-22 10:55:48 -0700

    adjusting session variables

--- Dashboard/verify_role.php
+++ Dashboard/verify_role.php
@@ -4,11 +4,11 @@ session_start();
 // Function to check access
 function checkAccess($requiredRole, $requiredTerritory = null) {
     if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-        header("Location: login.php");
+       // header("Location: login.php");
         exit;
     }
     if ($_SESSION['role'] !== $requiredRole || ($requiredTerritory !== null && $_SESSION['territory'] !== $requiredTerritory)) {
-        header("Location: no_access.php");
+       // header("Location: no_access.php");
         exit;
     }
 }
