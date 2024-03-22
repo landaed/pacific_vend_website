296dc886185ca1f44cd20eb5c6f620a87f4a44f0
DESKTOP-TJFGPKO\Eli Landa <elanda@wooplagaming.com>
2024-03-22 12:59:29 -0700

    fixing nav

--- Dashboard/get_session_info.js
+++ Dashboard/get_session_info.js
@@ -2,6 +2,7 @@ async function fetchSessionInfo() {
     try {
         let response = await fetch('session_info.php');
         let data = await response.json();
+        console.log('Session info:', data);
         return data;
     } catch (error) {
         console.error('Error:', error);
