c24c31dcac705a7cfba79a7ab9d72da8d36e3db5
DESKTOP-TJFGPKO\Eli Landa <elanda@wooplagaming.com>
2024-03-22 11:30:38 -0700

    adjusting session variables

--- Dashboard/topbar.js
+++ Dashboard/topbar.js
@@ -179,7 +179,7 @@ async function initializeTopbar() {
                     <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <span class="mr-2 d-none d-lg-inline text-gray-600 small">
-                            ${window.username}
+                            ${sessionData.username}
                         </span>
                         <img class="img-profile rounded-circle"
                             src="img/undraw_profile.svg">
